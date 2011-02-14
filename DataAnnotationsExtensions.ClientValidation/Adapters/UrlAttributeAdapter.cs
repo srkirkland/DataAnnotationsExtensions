5efb954d095ec1e130a5786d9d385c9fb200d750
Scott Kirkland <srkirkland@gmail.com>
2011-02-13 21:37:58 -0800

    Added a client validation project with client extensions for email and url

---
+++ DataAnnotationsExtensions.ClientValidation/Adapters/UrlAttributeAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DataAnnotationsExtensions.ClientValidation.Rules;
+
+namespace DataAnnotationsExtensions.ClientValidation.Adapters
+{
+    public class UrlAttributeAdapter : DataAnnotationsModelValidator<UrlAttribute>
+    {
+        public UrlAttributeAdapter(ModelMetadata metadata, ControllerContext context, UrlAttribute attribute)
+            : base(metadata, context, attribute)
+        {
+        }
+
+        public override IEnumerable<ModelClientValidationRule> GetClientValidationRules()
+        {
+            return new[] { new ModelClientValidationUrlRule(ErrorMessage) };
+        }
+    }
+
+    public class EmailAttributeAdapter : DataAnnotationsModelValidator<EmailAttribute>
+    {
+        public EmailAttributeAdapter(ModelMetadata metadata, ControllerContext context, EmailAttribute attribute)
+            : base(metadata, context, attribute)
+        {
+        }
+
+        public override IEnumerable<ModelClientValidationRule> GetClientValidationRules()
+        {
+            return new[] { new ModelClientValidationEmailRule(ErrorMessage) };
+        }
+    }
+}
---
+++ DataAnnotationsExtensions.ClientValidation/Rules/ModelClientValidationEmailRule.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DataAnnotationsExtensions.ClientValidation.Rules
+{
+    public class ModelClientValidationEmailRule : ModelClientValidationRule
+    {
+        public ModelClientValidationEmailRule(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+            ValidationType = "email";
+        }
+    }
+}
