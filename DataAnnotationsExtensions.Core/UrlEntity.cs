b353fffbfbf2eae167a15bdac9e73ad654b85ad6
Scott Kirkland <srkirkland@gmail.com>
2011-02-13 22:34:35 -0800

    refactored the url entity class into its own file

---
+++ DataAnnotationsExtensions.Core/UrlEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationsExtensions.Core
+{
+    public class UrlEntity
+    {
+        [Url]
+        [Required]
+        public string Url { get; set; }
+    }
+}
