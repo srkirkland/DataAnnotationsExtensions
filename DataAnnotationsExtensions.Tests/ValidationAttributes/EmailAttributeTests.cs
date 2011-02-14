c1354418b4fb9c2cfd406d247a156a93099f7036
Scott Kirkland <srkirkland@gmail.com>
2011-02-13 21:27:12 -0800

    Added email attribute server validation and test

---
+++ DataAnnotationsExtensions.Tests/ValidationAttributes/EmailAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAnnotationsExtensions.Tests.ValidationAttributes
+{
+    [TestClass]
+    public class EmailAttributeTests
+    {
+        [TestMethod]
+        public void IsValidTests()
+        {
+            var attribute = new EmailAttribute();
+
+            Assert.IsTrue(attribute.IsValid(null)); // Don't check for required
+            Assert.IsTrue(attribute.IsValid("foo@bar.com"));
+            Assert.IsTrue(attribute.IsValid("foo.bar@baz.com"));
+            Assert.IsTrue(attribute.IsValid("foo%bar@baz.com"));
+            Assert.IsFalse(attribute.IsValid("foo"));
+            Assert.IsFalse(attribute.IsValid("foo@"));
+            Assert.IsFalse(attribute.IsValid("foo@bar"));
+        }
+    }
+}
