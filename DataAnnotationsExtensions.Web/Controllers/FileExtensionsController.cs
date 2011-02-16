2eec7bd1f3e164b9b18ef3caef34b783177ea8ca
Scott Kirkland <srkirkland@gmail.com>
2011-02-15 19:42:22 -0800

    Added help text for the file extension and credit card pages

--- DataAnnotationsExtensions.Web/Controllers/FileExtensionsController.cs
+++ DataAnnotationsExtensions.Web/Controllers/FileExtensionsController.cs
@@ -4,5 +4,10 @@ namespace DataAnnotationsExtensions.Web.Controllers
 {
     public class FileExtensionsController : ValidationControllerBase<FileExtensionsEntity>
     {
+        protected override void AddMessage()
+        {
+            Message =
+                "File Extension Validation:  For the ImageFileName input the following extensions are valid {png,jpg,jpeg,gif}.  For the DocumentFileName input, the following extensions are valid {doc,docx,txt,rtf,pdf}";
+        }
     }
 }
