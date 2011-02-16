e579390e2d86f135a639f2c22dc9c846ece6c8e5
Scott Kirkland <srkirkland@gmail.com>
2011-02-15 17:40:43 -0800

    added client side validation rules and adapters for the numeric validator

---
+++ DataAnnotationsExtensions.ClientValidation/Adapters/NumericAttributeAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DataAnnotationsExtensions.ClientValidation.Rules;
+
+namespace DataAnnotationsExtensions.ClientValidation.Adapters
+{
+    public class NumericAttributeAdapter : DataAnnotationsModelValidator<NumericAttribute>
+    {
+        public NumericAttributeAdapter(ModelMetadata metadata, ControllerContext context, NumericAttribute attribute)
+            : base(metadata, context, attribute)
+        {
+        }
+
+        public override IEnumerable<ModelClientValidationRule> GetClientValidationRules()
+        {
+            return new[] { new ModelClientValidationNumericRule(ErrorMessage) };
+        }
+    }
+}
---
+++ DataAnnotationsExtensions.Core/NumericEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAnnotationsExtensions.Core
+{
+    public class NumericEntity
+    {
+        [Numeric]
+        public string Number { get; set; }
+    }
+}
