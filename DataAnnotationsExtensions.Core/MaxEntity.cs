7533fc059b8d2c687e04f439fbc0937d78cc7942
Scott Kirkland <srkirkland@gmail.com>
2011-02-15 20:37:37 -0800

    added example controller and views for max/min attributes, server-side only

---
+++ DataAnnotationsExtensions.Core/MaxEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAnnotationsExtensions.Core
+{
+    public class MaxEntity
+    {
+        [Max(50)]
+        public int MaxFifty { get; set; }
+
+        [Max(10.5)]
+        public double MaxTenAndAHalf { get; set; }
+    }
+}
---
+++ DataAnnotationsExtensions.Core/MinEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAnnotationsExtensions.Core
+{
+    public class MinEntity
+    {
+        [Min(50)]
+        public int MinFifty { get; set; }
+
+        [Min(10.5)]
+        public double MinTenAndAHalf { get; set; }
+    }
+}
