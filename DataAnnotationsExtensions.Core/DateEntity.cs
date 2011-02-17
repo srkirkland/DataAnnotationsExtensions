002db7c449b7ff67e6f66fc961976547e05c2ce8
Scott Kirkland <srkirkland@gmail.com>
2011-02-16 17:13:40 -0800

    created a controller and view for testing dates

---
+++ DataAnnotationsExtensions.Core/DateEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataAnnotationsExtensions.Core
+{
+    public class DateEntity
+    {
+        [Date]
+        public string DateAsString { get; set; }
+
+        [Date]
+        public DateTime DateTime { get; set; }
+
+        public DateEntity()
+        {
+            DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ DataAnnotationsExtensions.Web/Controllers/DateController.cs
@@ -0,0 +1,13 @@
+﻿using DataAnnotationsExtensions.Core;
+
+namespace DataAnnotationsExtensions.Web.Controllers
+{
+    public class DateController : ValidationControllerBase<DateEntity>
+    {
+        protected override void AddMessage()
+        {
+            Message =
+                "Both fields expect a date formatted string.  The DateAsString field shows server-side validation separate from data type checking";
+        }
+    }
+}
