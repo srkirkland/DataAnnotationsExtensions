7e73e2f44d6409f12c000a242f6df97ec313a024
Scott Kirkland <srkirkland@gmail.com>
2011-02-18 22:03:18 -0800

    fixed up the nuget packages in advance of deployment and added required to most examples

--- DataAnnotationsExtensions.Core/FileExtensionsEntity.cs
+++ DataAnnotationsExtensions.Core/FileExtensionsEntity.cs
@@ -1,11 +1,14 @@
-﻿namespace DataAnnotationsExtensions.Core
+﻿using System.ComponentModel.DataAnnotations;
+namespace DataAnnotationsExtensions.Core
 {
     public class FileExtensionsEntity
     {
         [FileExtensions] //Using defaults
+        [Required]
         public string ImageFileName { get; set; }
 
         [FileExtensions("doc|docx|txt|rtf|pdf")]
+        [Required]
         public string DocumentFileName { get; set; }
     }
 }
