276b8f69c1f88d872b4a2043e0031ce4934e4d79
Scott Kirkland <srkirkland@gmail.com>
2011-02-18 20:30:32 -0800

    Added nuget packages

---
+++ Build/NuGet/DataAnnotationsExtensions-MVC3/content/AppStart_RegisterClientValidationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using DataAnnotationsExtensions.ClientValidation;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.AppStart_RegisterClientValidationExtensions), "Start", callAfterGlobalAppStart: true)]
+ 
+namespace $rootnamespace$ {
+    public static class AppStart_RegisterClientValidationExtensions {
+        public static void Start() {
+            DataAnnotationsModelValidatorProviderExtensions.RegisterValidationExtensions();            
+        }
+    }
+}
