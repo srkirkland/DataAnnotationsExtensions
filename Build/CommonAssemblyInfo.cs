9ac4755e04bc0a38352a5264cfd500550ab150e6
Scott Kirkland <srkirkland@gmail.com>
2011-02-28 16:39:09 -0800

    Built a new NuGet version which includes a change to the web activator code to conform to David Ebbo's new standard

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
