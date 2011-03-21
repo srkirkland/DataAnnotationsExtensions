bfc3ab125f042537e04cc2d3e61c4785e3e4b378
Scott Kirkland <srkirkland@gmail.com>
2011-03-21 08:21:54 -0700

    updated to use the webactivator change to the pre application startup attribute ctor.  Released version 0.5

--- Build/NuGet/DataAnnotationsExtensions.MVC3/Content/App_Start/RegisterClientValidationExtensions.cs.pp
+++ Build/NuGet/DataAnnotationsExtensions.MVC3/Content/App_Start/RegisterClientValidationExtensions.cs.pp
@@ -1,6 +1,6 @@
 ﻿using DataAnnotationsExtensions.ClientValidation;
 
-[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.RegisterClientValidationExtensions), "Start", callAfterGlobalAppStart: true)]
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.RegisterClientValidationExtensions), "Start")]
  
 namespace $rootnamespace$.App_Start {
     public static class RegisterClientValidationExtensions {
