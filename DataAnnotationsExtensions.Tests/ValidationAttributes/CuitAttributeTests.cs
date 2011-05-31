fcb58c180bbdecf9c5779667c6baec8d758ec9f1
unknown <Rodrigo@.(none)>
2011-05-31 18:56:43 -0300

    CUIT Attribute - Missing Files

---
+++ DataAnnotationsExtensions.Tests/ValidationAttributes/CuitAttributeTests.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAnnotationsExtensions.Tests.ValidationAttributes
+{
+	[TestClass]
+	public class CuitAttributeTests
+	{
+		[TestMethod]
+		public void IsValidTests()
+		{
+			var attribute = new CuitAttribute();
+
+			Assert.IsTrue(attribute.IsValid(null));  
+			Assert.IsTrue(attribute.IsValid("20245597151"));
+			Assert.IsTrue(attribute.IsValid("20-24559715-1"));
+			Assert.IsTrue(attribute.IsValid("27-23840320-6"));
+			Assert.IsTrue(attribute.IsValid("27238403206"));
+			
+			Assert.IsFalse(attribute.IsValid("20 24559715 1"));
+            Assert.IsFalse(attribute.IsValid("")); 
+            Assert.IsFalse(attribute.IsValid("99-99999999-9"));
+			Assert.IsFalse(attribute.IsValid("99999999999"));
+			Assert.IsFalse(attribute.IsValid("4408 0412 3456 7890"));
+			Assert.IsFalse(attribute.IsValid(0));
+            Assert.IsFalse(attribute.IsValid(20245597151));
+		}
+
+        [TestMethod]
+        public void ErrorResourcesTest()
+        {
+            var attribute = new CuitAttribute();
+            attribute.ErrorMessageResourceName = "ErrorMessage";
+            attribute.ErrorMessageResourceType = typeof (ErrorResources);
+
+            const int invalidValue = 0;
+
+            var result = attribute.GetValidationResult(invalidValue, new ValidationContext(0, null, null));
+
+            Assert.AreEqual(ErrorResources.ErrorMessage, result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ErrorMessageTest()
+        {
+            var attribute = new CuitAttribute();
+            attribute.ErrorMessage = "SampleErrorMessage";
+            
+            const int invalidValue = 0;
+
+            var result = attribute.GetValidationResult(invalidValue, new ValidationContext(0, null, null));
+
+            Assert.AreEqual("SampleErrorMessage", result.ErrorMessage);
+        }
+	}
+}
---
+++ DataAnnotationsExtensions/CuitAttribute.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using DataAnnotationsExtensions.Resources;
+
+namespace DataAnnotationsExtensions
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CuitAttribute : DataTypeAttribute
+    {
+        public CuitAttribute()
+            : base("cuit")
+        {
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            if (ErrorMessage == null && ErrorMessageResourceName == null)
+            {
+                ErrorMessage = ValidatorResources.CuitAttribute_Invalid;
+            }
+
+            return base.FormatErrorMessage(name);
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            try
+            {
+                string cuit = (string)value;
+                List<int> prefijosValidos = new List<int>(new[] { 20, 22, 23, 27, 30 });
+
+                int preFijo = 0;
+                int dni = 0;
+                int control = 0;
+                if (cuit.Length == 11 || cuit.Length == 13)
+                {
+                    if (cuit.Contains("-"))
+                    {
+                        string[] aCuit = cuit.Split('-');
+                        preFijo = int.Parse(aCuit[0]);
+                        dni = int.Parse(aCuit[1]);
+                        control = int.Parse(aCuit[2]);
+                    }
+                    else
+                    {
+                        preFijo = int.Parse(cuit.Substring(0, 2));
+                        dni = int.Parse(cuit.Substring(2, 8));
+                        control = int.Parse(cuit.Substring(10, 1));
+                    }
+                }
+
+                string prefijoDni = preFijo + dni.ToString().PadLeft(8, '0');
+                int suma = 0;
+                int n;
+
+                if (!prefijosValidos.Contains(preFijo))
+                    return false;
+
+                //Algoritmo de calculo del digito verificador
+                const string coef = "5432765432";
+                for (n = 0; n < 10; n++)
+                    suma += int.Parse(prefijoDni.Substring(n, 1)) * int.Parse(coef.Substring(n, 1));
+
+                int resto = suma % 11;
+                int digitoVerificador;
+
+                if (resto == 0)
+                    digitoVerificador = 0;
+
+                else if (resto == 10)
+                    digitoVerificador = 1;
+
+                else
+                    digitoVerificador = 11 - resto;
+
+                return (digitoVerificador == control);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
