b1a6bbad34696663555577c0d16c20ee2ae5b0dd
Robb Schiefer <rschiefer@gmail.com>
2012-03-20 11:22:08 -0500

    DatePicker on param didn't work on 'blur' event;  changed to 'change' event

--- DataAnnotationsExtensions.Web/Scripts/jquery.validate.additional-methods.js
+++ DataAnnotationsExtensions.Web/Scripts/jquery.validate.additional-methods.js
@@ -1,5 +1,5 @@
 ﻿jQuery.validator.addMethod("greaterThan", function (value, element, param) {
-    var target = $(param).unbind(".validate-greaterThan").bind("blur.validate-greaterThan", function () {
+    var target = $(param).unbind(".validate-greaterThan").bind("change.validate-greaterThan", function () {
             $(element).valid();
         });
     return (Date.parse(value) || parseFloat(value)) > (Date.parse($(param).val()) || parseFloat($(param).val()));
