deb9722ae07c35f948403f0a75511a4feaef26ab
Jonathon Smith <jonathon.smith@ukf.net>
2012-04-10 00:35:16 +0100

    File extension validation now works with HttpPostedFileBase

---
+++ DataAnnotationsExtensions.Tests/Doubles/HttpPostedFileBaseStub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace DataAnnotationsExtensions.Tests.Doubles
+{
+    public class HttpPostedFileBaseStub : HttpPostedFileBase
+    {
+        public HttpPostedFileBaseStub(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        private string _fileName { set; get; }
+        public override string FileName { 
+            get
+            {
+                return _fileName;
+            } 
+        }
+    }
+}
