dd0ae132f7a335b58bca45f42a0a9cd9e8639782
Justin <justin@wirestorm.us>
2012-06-07 01:05:29 -0700

    Add: RequiredHttpAttribute.cs - provides the same functionality as RequiredAttribute.cs and extends it with an optional parameter to only validate on particular HTTP methods. This is extremely useful when designing a controller that handles a REST interface as the Create and Update methods should use the same model but may have different Required fields. This behaves identically to Require if no HttpMethod is specified. Usage: [RequiredHttp(HttpMethod = "POST")].
    Add: Regular expressions for currency codes, country codes, and culture codes - these validate structural correctness based on ISO 4217, RFC 4646, ISO 1366, and ISO 639. These do not validate existence.
    
    Add: Regular expression for phone numbers - this is a reasonable general purpose phone number regular expression. It allows for a large number of incorrect formats but it ensures only digits are entered and that non-digit characters show up in permitted places and matching in the case of parenthesis. This is a good first line of validation to catch nonsense or typos.

--- DataAnnotationsExtensions/Expressions.cs
+++ DataAnnotationsExtensions/Expressions.cs
@@ -3,5 +3,33 @@
     public static class Expressions
     {
         public const string Cuit = @"^[0-9]{2}-?[0-9]{8}-?[0-9]$";
+
+        /// <summary>
+        /// Matches:
+        ///   Country code: {{}, 0, ..., 0000, +0, ..., +0000} (with or without a trailing break character: [-/. ])
+        ///   Area code: {(00), ..., (0000), 00, ..., 0000}
+        ///   Local: {0, ...}+ (with or without a leading break character: [-/. ])
+        /// </summary>
+        public const string PhoneNumber = @"^(\+?\d{1,4}[-/.\s]?)?(\(\d{2,4}\)|\d{2,4})([-/.\s]?\d+)+$";
+
+        /// <summary>
+        /// ISO 4217:2008: Currency codes. Confirms structure not validity.
+        /// </summary>
+        public const string CurrencyCode = @"^[a-zA-Z]{3}|[0-9]{3}$";
+
+        /// <summary>
+        /// ISO 3166: Country codes. Confirms structure not validity.
+        /// </summary>
+        public const string CountryCode = @"^[a-zA-Z]{2,3}|[0-9]{3}$";
+
+        /// <summary>
+        /// RFC 4646: ISO 639 + ISO 3166: Culture codes. Confirms structure not validity.
+        /// Matches:
+        ///   Neutral culture: {aa, ..., ZZZ}
+        ///   Neutral culture with script: {aa-aaaa, ..., ZZZ-ZZZZ}
+        ///   Specific culture: {aa-aa, ..., ZZZ-ZZZ, aa-000, ... ZZZ-000}
+        ///   Specific culture with script: {aa-aa-aaaa, ..., ZZZ-ZZZ-ZZZZ, aa-000-aaaa, ..., ZZZ-000-ZZZZ}
+        /// </summary>
+        public const string CultureCode = @"^[a-zA-Z]{2,3}(-([a-zA-Z]{2,3}|[0-9]{3}))?(-[a-zA-Z]{4})?$";
     }
 }
---
+++ DataAnnotationsExtensions/RequiredHttpAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace DataAnnotationsExtensions
+{
+    /// <summary>
+    /// RequiredHttpAttribute class. Identical behavior to the RequiredAttribute class
+    /// with the additon that it allows for bypassing validation based on the Http
+    /// method of the current Http request.
+    /// </summary>
+    [AttributeUsageAttribute(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class RequiredHttpAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute
+    {
+        /// <summary>
+        /// Gets or sets the HTTP methods that this attribute will validate on.
+        /// </summary>
+        /// <remarks>The value is treated as a regular expression pattern, e.g., GET|POST.</remarks>
+        public string HttpMethod { get; set; }
+
+        /// <summary>
+        /// Validates the specified value with respect to the current validation attribute.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <param name="validationContext">The context information about the validation operation.</param>
+        /// <returns>An instance of the ValidationResult class.</returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var context = HttpContext.Current;
+            if (context != null && !string.IsNullOrWhiteSpace(this.HttpMethod))
+            {
+                if (!Regex.IsMatch(context.Request.HttpMethod, this.HttpMethod, RegexOptions.IgnoreCase))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
