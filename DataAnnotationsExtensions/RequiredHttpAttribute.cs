84c80c71dd51ea49187fea32f94994fcd0952872
Justin <justinsa@live.ca>
2012-08-10 11:50:36 -0700

    Added RequiredHttpAttribute unit tests and fixed issue for IsValid(object value) signature
    * Added unit tests for testing the RequiredHttpAttribute class
    * Added override for IsValid(object value) function (discovered because
    of unit tests)

--- DataAnnotationsExtensions/RequiredHttpAttribute.cs
+++ DataAnnotationsExtensions/RequiredHttpAttribute.cs
@@ -20,6 +20,17 @@ namespace DataAnnotationsExtensions
         public string HttpMethod { get; set; }
 
         /// <summary>
+        /// Validates the specified value.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <returns>True if the value is valid and false otherwise.</returns>
+        public override bool IsValid(object value)
+        {
+            var required = this.RequiredInContext(HttpContext.Current);
+            return required ? base.IsValid(value) : true;
+        }
+
+        /// <summary>
         /// Validates the specified value with respect to the current validation attribute.
         /// </summary>
         /// <param name="value">The value to validate.</param>
@@ -27,16 +38,26 @@ namespace DataAnnotationsExtensions
         /// <returns>An instance of the ValidationResult class.</returns>
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var context = HttpContext.Current;
+            var required = this.RequiredInContext(HttpContext.Current);
+            return required ? base.IsValid(value, validationContext) : ValidationResult.Success;
+        }
+
+        /// <summary>
+        /// Determines if the provided HTTP context permits validation.
+        /// </summary>
+        /// <param name="context">The HTTP context.</param>
+        /// <returns>True if validation is required and false otherwise.</returns>
+        protected bool RequiredInContext(HttpContext context)
+        {
             if (context != null && !string.IsNullOrWhiteSpace(this.HttpMethod))
             {
                 if (!Regex.IsMatch(context.Request.HttpMethod, this.HttpMethod, RegexOptions.IgnoreCase))
                 {
-                    return ValidationResult.Success;
+                    return false;
                 }
             }
 
-            return base.IsValid(value, validationContext);
+            return true;
         }
     }
 }
