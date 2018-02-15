edab771e99bb171bf1b517c4e2fa437511a2d847
Unknown <srkirkland@ucdavis.edu>
2018-02-14 16:59:11 -0800

    test library

---
+++ test/DataAnnotationExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAnnotationExtensions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
