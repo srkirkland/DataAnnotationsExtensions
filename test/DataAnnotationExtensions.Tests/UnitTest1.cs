5b8cbed5978a2e4fdb920c7b01994f7de582275b
Scott Kirkland <srkirkland@gmail.com>
2018-03-13 16:12:26 -0700

    redo sln, csproj, and get resources

--- test/DataAnnotationExtensions.Tests/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace DataAnnotationExtensions.Tests
-{
-    public static class ErrorResources {
-        public static string ErrorMessage = "error message";
-    }
-}
